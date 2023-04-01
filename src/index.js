c0f67343cbe3e9db8334cbcb860578cafaa8ae2f
Manuel Araujo <manuelmad1981@gmail.com>
2023-04-01 13:13:44 -0400

    Mejorando apariencia de celebración: Fireworks y varias imágenes de videojuegos.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ function chooseWinNer() {
 		document.getElementById('winner').innerText = '';
 		document.getElementById('button').disabled = true;
 		document.getElementById('button').style.backgroundColor = '#e9e8e8';
+		document.getElementById('fireworks').style.display = 'none';
 		document.getElementById('gif-container').style.display = 'none';
 		window.location.hash = "winner";
 
@@ -28,26 +29,47 @@ function chooseWinNer() {
 			//console.log(indice_random);
 			let nombre_random = participantes[indice_random];
 			//console.log(nombre_random);
-			document.getElementById('winner').innerText = `
-
-			${nombre_random}
-
-			`;
-
+			document.getElementById('winner').innerText = `${nombre_random}`;
 		},200);
 
 		// Detengo el proceso anterior, hago aparecer el nombre del ganador elegido y activo el botón nuevamente
 		setTimeout(() => {
 			clearInterval(random);
-			document.getElementById('winner').innerText = `🥳🎉🥳🎉🥳
-
-			(${indice_ganador+1}) ${nombre_ganador}
-
-			🥳🎉🥳`;
+			document.getElementById('winner').innerText = `N° ${indice_ganador+1}
+			${nombre_ganador}`;
 			document.getElementById('button').disabled = false;
 			document.getElementById('button').style.backgroundColor = '#03fbfa';
+			document.getElementById('fireworks').style.display = 'block';
 			document.getElementById('gif-container').style.display = 'block';
 			window.location.hash = "gif-container";
+			giftshower();
 		}, 5000);
 	}
+}
+
+// Funcióm para alternar gifs
+const img = document.getElementById('gif');
+
+const url1 = '../assets/imgs/mario-luigi.webp';
+const url2 = '../assets/imgs/zelda.gif';
+const url3 = '../assets/imgs/pikachu-sparkle.gif';
+const url4 = '../assets/imgs/sonic2.gif';
+const url5 = '../assets/imgs/crash-bandicoot.gif';
+
+let urls_array = [url1, url2, url3, url4, url5];
+
+let counter = 0;
+
+function rotateImg() {
+	img.src = urls_array[counter];
+	if(counter >= urls_array.length - 1) {
+		counter = 0;
+	} else {
+		counter++;
+	}
+}
+
+function giftshower() {
+	rotateImg();
+	setInterval(rotateImg,3000);
 }
